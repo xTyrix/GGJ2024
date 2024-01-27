f819ebfe071896842649c4a47c189d8f062edf72
Addis Dittebrandt <addis.dittebrandt@posteo.net>
2024-01-27 19:42:41 +0100

    use input event for mouse click

--- godot/scenes/Clickable.gd
+++ godot/scenes/Clickable.gd
@@ -5,18 +5,11 @@ signal pressed
 
 @export var active = true
 
-var mouse_hover = false
-
-func _input(event):
-	if not (active and mouse_hover):
+func _on_input_event(viewport, event, shape_idx):
+	print("Hello")
+	if not active:
 		return
 
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			pressed.emit()
-
-func _on_mouse_entered():
-	mouse_hover = true
-
-func _on_mouse_exited():
-	mouse_hover = false
