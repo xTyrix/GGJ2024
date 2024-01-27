e5f5afbf1a8cd2f0d5464ae6e72c009d9db35af3
Addis Dittebrandt <addis.dittebrandt@posteo.net>
2024-01-27 17:23:52 +0100

    update inventory area position

--- godot/scenes/InventoryArea.gd
+++ godot/scenes/InventoryArea.gd
@@ -2,16 +2,18 @@ extends Node2D
 
 var inventory_extended = false
 
+var offset = Vector2(350, 350)
+
 func _ready():
-	position += Vector2(100,100)
+	position += offset
 
 func _on_area_2d_mouse_entered():
 	if not inventory_extended:
-		position -= Vector2(100,100)
+		position -= offset
 		inventory_extended = true
 
 
 func _on_area_2d_2_mouse_exited():
 	if  inventory_extended:
-		position += Vector2(100,100)
+		position += offset
 		inventory_extended = false
