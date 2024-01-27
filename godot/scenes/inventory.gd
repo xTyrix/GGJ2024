0b08935043eb082d65b459e7036755db1a3403a1
Addis Dittebrandt <addis.dittebrandt@posteo.net>
2024-01-27 11:25:51 +0100

    exchange sprites in inventory

--- godot/scenes/inventory.gd
+++ godot/scenes/inventory.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var active_sprite = null
+var active_sprite: Node = null
+var active_slot: Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,7 +15,6 @@ func _process(delta):
 		var pos = get_viewport().get_mouse_position();
 		active_sprite.position = pos + Vector2(15, 15)
 
-
 func add_to_inventory(sprite):
 	for n in 10:
 		var node = get_node("%s" % n)
@@ -26,6 +26,14 @@ func add_to_inventory(sprite):
 
 func _on_slot_pressed(node):
 	if node.get_child_count() > 1:
+		var old_active_sprite = active_sprite
+			
 		active_sprite = node.get_child(1)
 		active_sprite.get_parent().remove_child(active_sprite)
 		add_child(active_sprite)
+
+		if old_active_sprite:
+			old_active_sprite.position = Vector2(0, 0)
+			old_active_sprite.get_parent().remove_child(old_active_sprite)
+			node.add_child(old_active_sprite)
+			
