4e36e4a5fad0c94fcdb90d6c51aa7f153b205498
Patrick Exner <patrick.exner1@web.de>
2024-01-28 15:28:56 +0100

    Win Screen verbunden

--- godot/scenes/level/Fuss_meter.gd
+++ godot/scenes/level/Fuss_meter.gd
@@ -1,22 +1,26 @@
 extends Node
 
+var tickle1 = 3
+var tickle2 = 3
+var tickle3 = 3
 
-func test_win():
-	if tickle1 + tickle2 + tickle3 == 0:
-		print("you have won this level")
+signal puzzle_solved
+
+var win_screen_visible: bool = false
+
+func win_screen():
+	if tickle1 + tickle2 + tickle3 == 0 and win_screen_visible == false:
+		print("you have solved this level")
+		emit_signal("puzzle_solved")
+		win_screen_visible = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
-var tickle1 = 3
-var tickle2 = 3
-var tickle3 = 3
+	win_screen()
 
 func reset():
 	tickle1 = 3
@@ -27,7 +31,6 @@ func reset():
 func _on_fuss_tickle_1_pressed():
 	if get_parent().get_node("Inventory").active_sprite_has_name("Feder"):
 		if tickle1 <= 0:
-			test_win()
 			return
 		tickle1 -= 1
 		$VSlider.value += 10
@@ -37,7 +40,6 @@ func _on_fuss_tickle_1_pressed():
 func _on_fuss_tickle_2_pressed():
 	if get_parent().get_node("Inventory").active_sprite_has_name("Sonnenblume"):
 		if tickle2 <= 0:
-			test_win()
 			return
 		tickle2 -= 1
 		$VSlider.value += 10
@@ -47,7 +49,6 @@ func _on_fuss_tickle_2_pressed():
 func _on_fuss_tickle_3_pressed():
 	if get_parent().get_node("Inventory").active_sprite_has_name("Kaktus"):
 		if tickle3 <= 0:
-			test_win()
 			return
 		tickle3 -= 1
 		$VSlider.value += 10
