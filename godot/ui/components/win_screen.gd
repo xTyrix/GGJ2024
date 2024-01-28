17f3c9ace831270a485b4deb5ec76b002d238893
Patrick Exner <patrick.exner1@web.de>
2024-01-28 15:55:30 +0100

    Win Screen Layer Scene mit Code

---
+++ godot/ui/components/win_screen.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@export var connected_node_path: NodePath
+@export var win_change_to_scene: PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var signal_node: Node = get_node(connected_node_path)
+	signal_node.connect("puzzle_solved", _set_visible)
+
+func _set_visible():
+	visible = true
+
+func _unhandled_input(event):
+	if visible == true and event == InputEventKey:
+		if event.is_action_pressed("ui_accept"):
+			get_tree().change_scene_to_packed(win_change_to_scene)
