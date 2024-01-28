acaef3c9df03e1d3eb9134cb04bb202216ca1c56
Anastylos <noname@noemail.com>
2024-01-28 12:28:56 +0100

    koenig animation

--- godot/scenes/level/Koenig.gd
+++ godot/scenes/level/Koenig.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	$Koenig/AnimationPlayer.play("king-idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
